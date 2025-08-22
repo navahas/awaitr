fb9376e2b18cf67a6808e1a2a4b20af8418d9cdb
Carlos Navajas <c.navajas4@gmail.com>
2025-08-22 15:21:13 +0200

    feat: improved showcase and code cleanup

--- src/main.rs
+++ src/main.rs
@@ -8,16 +8,21 @@ const REGULAR: &str = "\x1b[22m";
 
 const GRAY: &str = "\x1B[38;2;136;136;136m";
 const RED: &str = "\x1B[91m";
-const BRIGHT_ORANGE: &str = "\x1b[38;2;235;158;127m";
+const BRIGHT_RED: &str = "\x1b[38;2;235;158;127m";
 const GREEN: &str = "\x1b[38;2;163;190;140m";
 const BRIGHT_GREEN: &str = "\x1b[38;2;186;210;157m";
 const BLUE: &str = "\x1b[38;2;174;198;207m";
-const BRIGHT_BLUE: &str = "\x1b[38;2;167;199;231m";
+const BRIGHT_BLUE: &str = "\x1b[38;2;224;233;236m";
 const YELLOW: &str = "\x1b[38;2;241;216;165m";
 const BRIGHT_YELLOW: &str = "\x1b[38;2;240;223;174m";
 
 const HIDE_CURSOR: &str = "\x1B[?25l";
 const SHOW_CURSOR: &str = "\x1B[?25h";
+const FPS: u64 = 218;
+
+fn fps() {
+    sleep(Duration::from_millis(FPS));
+}
 
 fn word_loader(word: &str, color: &str, h_color: &str, frame: usize) -> String {
     let word_chars: Vec<char> = word.chars().collect();
@@ -30,54 +35,57 @@ fn word_loader(word: &str, color: &str, h_color: &str, frame: usize) -> String {
     word_frame
 }
 
+fn spinner_loader(icons: &str, color: &str, frame: usize) -> String {
+    let spinner_chars: Vec<char> = icons.chars().collect();
+    let spinner_frame = spinner_chars[frame % spinner_chars.len()];
+    let spinner = format!("{color}{spinner_frame}");
+    spinner
+}
+
+fn cast_loader(spinner: String, word: String) {
+    println!("\n");
+    println!("{:width$}{} {} {}", "", spinner, word, RESET, width = 11);
+}
+
 fn dummy_progress() {
     let tasks = 100;
 
-    let spinner = " ·•✤✻✶✼✽❃✹✺✹❇✶✻✤•·•✤❈❉❊✤✻✼✽❃✶✺✹❇❈❉❊✤•· ";
-    let spinner_chars: Vec<char> = spinner.chars().collect();
-    let spinner_len = spinner_chars.len();
-
+    let default_spinner = " ·•✤✻✶✼✽❃✹✺✹❇✶✻✤•·•✤❈❉❊✤✻✼✽❃✶✺✹❇❈❉❊✤•· ";
+    let spinner_a = "⣾⣽⣻⢿⡿⣟⣯⣷";
+    let spinner_b = " ▏▎▍▌▋▊▉▉▊▋▌▍▎";
+    let spinner_c = " ▁▂▃▄▅▆▇█▇▆▅▄▃▁ ";
 
     println!("{HIDE_CURSOR}");
     for t in 0..tasks {
-        let spinner_frame = spinner_chars[t % spinner_len];
+        let rustifying = word_loader("Rustifying…", RED, BRIGHT_RED, t);
+        let s_rustifying = spinner_loader(default_spinner, RED, t);
 
-        let rustifying = word_loader("Rustifying…", RED, BRIGHT_ORANGE, t);
         let compiling = word_loader("Compiling…", GREEN, BRIGHT_GREEN, t);
+        let s_compiling = spinner_loader(spinner_a, GREEN, t);
+
         let borrowchecking = word_loader("Borrowchecking…", BLUE, BRIGHT_BLUE, t);
+        let s_borrowchecking = spinner_loader(spinner_b, BLUE, t);
+
         let working = word_loader("Working…", YELLOW, BRIGHT_YELLOW, t);
+        let s_working = spinner_loader(spinner_c, YELLOW, t);
 
-        println!("{}\n", CLEAR);
-        println!(
-            "{:width$}{}{} {} {}",
-            "", RED, spinner_frame, rustifying, RESET, width = 11
-        );
+        println!("{}", CLEAR);
 
-        println!("\n");
-        println!(
-            "{:width$}{}{} {} {}",
-            "", GREEN, spinner_frame, compiling, RESET, width = 11
-        );
-
-        println!("\n");
-        println!(
-            "{:width$}{}{} {} {}",
-            "", BLUE, spinner_frame, borrowchecking, RESET, width = 11
-        );
-
-        println!("\n");
-        println!(
-            "{:width$}{}{} {} {}",
-            "", YELLOW, spinner_frame, working, RESET, width = 11
-        );
+        let _rusty_progress = cast_loader(s_rustifying, rustifying);
+        let _compiling_progress = cast_loader(s_compiling, compiling);
+        let _borrowchecking_progress = cast_loader(s_borrowchecking, borrowchecking);
+        let _working_progress = cast_loader(s_working, working);
 
         println!("\n");
         println!(
             "{:width$}{}(press crtl+C to exit){}",
-            "", GRAY, RESET, width = 11
+            "",
+            GRAY,
+            RESET,
+            width = 11
         );
 
-        sleep(Duration::from_millis(225));
+        fps();
     }
     println!("{SHOW_CURSOR}");
 }
