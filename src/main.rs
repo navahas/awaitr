267ddde4dac18932fd16cd3050fc0a593b07c679
Carlos Navajas <c.navajas4@gmail.com>
2025-08-22 14:08:38 +0200

    feat: dummy loader implemented

--- src/main.rs
+++ src/main.rs
@@ -2,48 +2,47 @@ use std::{thread::sleep, time::Duration};
 
 const CLEAR: &str = "\x1B[2J\x1B[1;1H";
 
-const GRAY: &str = "\x1B[37m";
+const GRAY: &str = "\x1B[38;2;136;136;136m";
 const RED: &str = "\x1B[91m";
-const BRIGHT_RED: &str = "\x1B[92m";
-const YELLOW: &str = "\x1B[93m";
 const RESET: &str = "\x1B[0m";
 const BOLD: &str = "\x1B[1m";
+const REGULAR: &str = "\x1b[22m";
 const BRIGHT_ORANGE: &str = "\x1b[38;2;235;158;127m";
 
 const HIDE_CURSOR: &str = "\x1B[?25l";
 const SHOW_CURSOR: &str = "\x1B[?25h";
 
-fn word_progress() {
-    let word = "Wizarding…";
+fn dummy_progress() {
+    let tasks = 100;
+
+    let word = "Rustifying…";
+    let spinner = " ·•✤✻✶✼✽❃✹✺✹❇✶✻✤•·•✤❈❉❊✤✻✼✽❃✶✺✹❇❈❉❊✤•· ";
+
     let word_chars: Vec<char> = word.chars().collect();
     let word_len = word_chars.len();
-    for letter in 0..word_len {
-        let position = letter % word_len;
-        //let frame = word_chars[position];
-        let left: String = word.chars().take(position).collect();
-        let mid = word_chars[position];
-        let right: String = word.chars().skip(position + 1).collect();
-        let frame = format!("{}{}{}{}{}{}", RED, left, BRIGHT_ORANGE, mid, RED, right);
-        println!("{}\n {}", CLEAR, frame);
-        sleep(Duration::from_millis(300));
-    }
-}
 
-fn progress() {
-    let tasks = 50;
-    let spinner = " ·•✤✻✶✼✽❃✹✺✹❇✶❈❉❊✤✻✼✽❃✶✺✹❇❈❉❊✤•· ";
     let spinner_chars: Vec<char> = spinner.chars().collect();
     let spinner_len = spinner_chars.len();
 
     println!("{HIDE_CURSOR}");
     for t in 0..tasks {
-        let frame = spinner_chars[t % spinner_len];
-        println!("{}\n      {}{} Wizarding… ", CLEAR, RED, frame);
-        sleep(Duration::from_millis(250));
+        let spinner_frame = spinner_chars[t % spinner_len];
+
+        let w_position = t % word_len;
+        let left: String = word.chars().take(w_position).collect();
+        let mid = word_chars[w_position];
+        let right: String = word.chars().skip(w_position + 1).collect();
+        let word_frame = format!("{RED}{left}{BRIGHT_ORANGE}{BOLD}{mid}{REGULAR}{RED}{right}");
+
+        println!("{}\n", CLEAR);
+        println!("      {}{} {} {}    (press ctrl+C to strop){}", RED, spinner_frame, word_frame, GRAY, RESET);
+        println!("\n");
+        println!("      {}{} {} {}    (press ctrl+C to strop){}", RED, spinner_frame, word_frame, GRAY, RESET);
+        sleep(Duration::from_millis(225));
     }
     println!("{SHOW_CURSOR}");
 }
 
 fn main() {
-    word_progress();
+    dummy_progress();
 }
